bb54f15956c6a2283cffad4e674152eaa18f92a1
ICT Student <ictstudent@icqm.edu.hk>
2017-07-11 15:39:34 +0800

    navbar

--- login.php
+++ login.php
@@ -19,9 +19,25 @@
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <!-- Compiled and minified JavaScript -->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.min.js"></script>     
-      
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.min.js"></script>
 
-      
+<nav>
+  <div class="nav-wrapper blue lighten-2">
+		<div class="row">
+			<div class="col s12">
+    		<a href="index.php" class="brand-logo hide-on-small-only flow-text">Eugene's Anime Archive</a>
+        <a href="index.php" class="brand-logo right hide-on-med-and-up flow-text">Eugene's Anime Archive</a>
+    		<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+    		<ul class="right hide-on-med-and-down">
+      		<li class="active"><a href="admin.php">Admin</a></li>
+    		</ul>
+    		<ul class="side-nav" id="mobile-demo">
+      		<li class="active"><a href="admin.php">Admin</a></li>
+    		</ul>
+			</div>
+		</div>
+  </div>
+</nav>
+
 </body>
 </html>
