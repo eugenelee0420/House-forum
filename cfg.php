b3d214e385bf02a7d119a959b6c798f14274cfbf
ICT Student <ictstudent@icqm.edu.hk>
2017-07-11 16:09:21 +0800

    add cfg.php

---
+++ cfg.php
@@ -0,0 +1,10 @@
+<?php
+// Configuration file
+// Contains all database credentials and settings
+
+$dbHost = "localhost";
+$dbUser = "id214994_ict";
+$dbPass = "eugenelee";
+$dbName = "id214994_ict";
+
+?>
