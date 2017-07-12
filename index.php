3c648e51e912929309eedd961be24ed9c676eff1
Eugene Lee <eugenelee0420@gmail.com>
2017-07-12 11:34:34 +0800

    Standard sidenav template

--- index.php
+++ index.php
@@ -53,3 +53,53 @@ $row = mysqli_fetch_assoc($result);
 echo "Student ID: ".$row['studentId']."<br>";
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<!--Import Google Icon Font-->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<!--Import materialize.css-->
+<!-- Compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/css/materialize.min.css">
+<!--Let browser know website is optimized for mobile-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.min.js"></script>
+
+<!-- Initialize mobile sidenav-->
+<script>
+$(document).ready(function() {
+		$(".button-collapse").sideNav();
+  });
+</script>
+
+<nav>
+  <div class="nav-wrapper purple lighten-2">
+		<div class="row">
+			<div class="col s12">
+    		<a href="index.php" class="brand-logo center flow-text">House Forums</a>
+				<ul id="slide-out" class="side-nav">
+					<li><div class="user-view">
+						<div class="background">
+							<img src="images/office.jpg">
+						</div>
+						<a href="#!user"><img class="circle" src="images/yuna.jpg"></a>
+						<a href="#!name"><span class="white-text name">John Doe</span></a>
+						<a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
+					</div></li>
+					<li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+					<li><a href="#!">Second Link</a></li>
+					<li><div class="divider"></div></li>
+					<li><a class="subheader">Subheader</a></li>
+					<li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+				</ul>
+				<a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="material-icons">menu</i></a>
+			</div>
+		</div>
+  </div>
+</nav>
