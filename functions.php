2b7d156e8a5976d1c7fea28e3d6929bfa2cdebf4
Eugene Lee <eugenelee0420@gmail.com>
2017-07-14 19:53:52 +0800

    reduce indent

--- functions.php
+++ functions.php
@@ -103,28 +103,28 @@ function echoGetUserHouseName($sessId) {
 // Function to get hId of current sessionId
 function getUserHId($sessId) {
 
-    require "cfg.php";
+  require "cfg.php";
 
-    // Connect to database
-    $conn = new mysqli($dbHost,$dbUser,$dbPass,$dbName);
-    if ($conn->connect_error) {
-      die('<font color="red">Connection failed: '.$conn->connect_error.'</font>');
-    }
-
-    $studentId = getStudentId($sessId);
-
-    $sql = 'SELECT hId from users WHERE studentId = "'.$studentId.'";';
-    $result = $conn->query($sql);
-    if (!$result) {
-      die('Query failed. '.$conn->error);
-    }
-
-    $row = mysqli_fetch_assoc($result);
-    return $row['hId'];
-
-    mysqli_free_result($result);
-
+  // Connect to database
+  $conn = new mysqli($dbHost,$dbUser,$dbPass,$dbName);
+  if ($conn->connect_error) {
+    die('<font color="red">Connection failed: '.$conn->connect_error.'</font>');
   }
+
+  $studentId = getStudentId($sessId);
+
+  $sql = 'SELECT hId from users WHERE studentId = "'.$studentId.'";';
+  $result = $conn->query($sql);
+  if (!$result) {
+    die('Query failed. '.$conn->error);
+  }
+
+  $row = mysqli_fetch_assoc($result);
+  return $row['hId'];
+
+  mysqli_free_result($result);
+
+}
 
 
 ?>
