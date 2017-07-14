685e87fc58b30ebe839a201015897cacb73070fa
Eugene Lee <eugenelee0420@gmail.com>
2017-07-14 16:35:01 +0800

    sample db credentials

--- cfg.php
+++ cfg.php
@@ -3,9 +3,9 @@
 // Contains all database credentials and settings
 
 $dbHost = "localhost";
-$dbUser = "id214994_ict";
-$dbPass = "eugenelee";
-$dbName = "id214994_ict";
+$dbUser = "forum";
+$dbPass = "password";
+$dbName = "forum";
 
 // Idle time before user is logged out automatically (seconds)
 $userTimeout = 600;
