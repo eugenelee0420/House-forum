8b6034e97b362b06af392b45261f019b5c75ca2d
Eugene Lee <eugenelee0420@gmail.com>
2017-08-15 16:58:34 +0800

    moved settings to db

--- cfg.php
+++ cfg.php
@@ -7,11 +7,4 @@ $dbUser = "forum";
 $dbPass = "password";
 $dbName = "forum";
 
-// Idle time before user is logged out automatically (seconds)
-$userTimeout = 600;
-
-// UNIX epoch timezone offset
-// Refer to https://www.epochconverter.com/timezones for more details
-$timezoneOffset = + 28800;
-
 ?>
