1f871f5c35352c88b25d5b12bb837cad306cd476
Eugene Lee <eugenelee0420@gmail.com>
2018-02-02 16:00:50 +0800

    finish html form

--- setup.php
+++ setup.php
@@ -1,7 +1,15 @@
 <?php
 // Setup script for first time use
-if (isset($_POST['submit'])) {
+if ($_POST['submit'] == "submit") {
   // Form submitted
+
+  echo 'Submitted data (PHP array):<br>';
+  echo '<pre>';
+  var_dump($_POST);
+  echo '</pre><br>';
+
+  
+
 
 } else {
 
@@ -17,6 +25,12 @@ if (isset($_POST['submit'])) {
   <!--Import materialize.css-->
   <!-- Compiled and minified CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css">
+
+  <!-- Highlight.js -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/atom-one-dark.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+  <script>hljs.initHighlightingOnLoad();</script>
+
   <!--Let browser know website is optimized for mobile-->
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   </head>
@@ -164,7 +178,111 @@ if (isset($_POST['submit'])) {
       <!-- Create houses
       Create new user (ultimate user group) -->
 
+      <h4>Houses</h4>
+      <p>These should not be changed later. New houses can be added directly to the database.</p>
 
+      <div class="row">
+        <div class="col s12">
+          <p>These are the houses which the users belong to. Each user must belong to one house.</p>
+          <p>Please use the JSON format to input the data. An example is shown below.</p>
+          <p>hId is an unique identifyer of the houses, while houseName is the displayed name of the house.</p>
+          <p>hId should be at most 3 characters long, houseName should be at most 20 characters long.</p>
+        </div>
+      </div>
+
+      <div class="row"><div class="col s12">
+        <p>Sample of the JSON data:</p>
+      <pre><code class="json">[
+  {
+    "hId":"Y",
+    "houseName":"Yellow"
+  },
+  {
+    "hId":"R",
+    "houseName":"Red"
+  }
+]</code></pre>
+      </div></div>
+
+      <div class="row">
+        <div class="input-field col s12">
+          <textarea id="house" name="house" class="materialize-textarea"></textarea>
+          <label for="house">Houses</label>
+        </div>
+      </div>
+
+      <h4>User group</h4>
+      <p>Create an "ultimate" user group that have every permission. Other user groups can be created later at the user group settings page.</p>
+
+      <div class="row">
+        <div class="col s12">
+          <p>Unique ID of the user group:</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="userGroup" name="userGroup" type="text" data-length="3">
+          <label for="userGroup">User group ID</label>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col s12">
+          <p>Name of the user group:</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="userGroupName" name="userGroupName" type="text" data-length="50">
+          <label for="userGroupName">User group name</label>
+        </div>
+      </div>
+
+      <h4>User</h4>
+      <p>Create a user for setting up rest of the forum. New users should be added using the add_user.php script.</p>
+      <p>This user will belong to the "ultimate" user group created above.</p>
+
+      <div class="row">
+        <div class="col s12">
+          <p>A unique ID for each user, used to login to the forum.</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="studentId" name="studentId" type="text" data-length="7">
+          <label for="studentId">Student ID</label>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="pass" name="pass" type="password">
+          <label for="pass">User password</label>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col s12">
+          <p>The house ID of the user:</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="userHId" name="userHId" type="text" data-length="3">
+          <label for="userHId">House ID (user)</label>
+        </div>
+      </div>
+
+      <button class="btn waves-effect purple waves-light" type="submit" name="submit" value="submit">Submit
+      <i class="material-icons right">send</i>
+
+
+  </form>
+</div>
 
 
   <?php
